08e638882a51c4f6c3bd6ff9d4ca1b28e8e406e2
Chanuka999 <chanukaranditha99@gmail.com>
2025-03-03 14:53:08 +0530

    added hero .jsx page

--- space_website_v2/src/App.jsx
+++ space_website_v2/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import video from "./assets/video.mp4";
 import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
 
 const App = () => {
   return (
@@ -10,11 +11,12 @@ const App = () => {
           autoPlay
           loop
           muted
-          className="fixed right-0 top-0 h-[700[px] w-full object-cover z[-1]"
+          className="fixed right-0 top-0 h-[700px] w-full object-cover z[-1]"
         >
           <source src={video} type="video/mp4" />
         </video>
         <Navbar />
+        <Hero />
       </div>
     </div>
   );
