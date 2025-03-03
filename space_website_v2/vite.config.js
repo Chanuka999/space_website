46cf3d4e821f4d802a21258c9c20da3bfef60833
Chanuka999 <chanukaranditha99@gmail.com>
2025-03-03 11:37:31 +0530

    pluging thaiwind css

--- space_website_v2/vite.config.js
+++ space_website_v2/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { Container } from "postcss";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  theme: {
+    extend: {
+      Container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+        },
+      },
+    },
+  },
+
+  plugins: [react(), tailwindcss()],
+});
