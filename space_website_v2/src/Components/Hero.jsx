957e75aaffc0c54ff7f9ed29acb1e1d602938560
Chanuka999 <chanukaranditha99@gmail.com>
2025-03-04 01:16:07 +0530

    update

--- space_website_v2/src/Components/Hero.jsx
+++ space_website_v2/src/Components/Hero.jsx
@@ -6,14 +6,14 @@ const Hero = () => {
       <div className="h-full flex justify-center items-center p-4">
         <div className="container grid grid-cols-1 sm:grid-cols-2 gap-4"></div>
         <div className="space-y-4 lg:pr-36">
-          <h1 className="text-5xl font-bold">Orbite the Earth</h1>
+          <h1 className="text-5xl font-bold uppercase">Orbite the Earth</h1>
           <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore
             vero tenetur nostrum ad pariatur! Aspernatur tempora, commodi
             voluptatem harum debitis culpaet sapiente placeat provident
             consectetur quibusdam dolores dignissimos maiores?
           </p>
-          <button className="bg-blue-400 text-white hover-bg-blue-500 px-4 py-1 rounded-md duration-200">
+          <button className="bg-blue-400 text-white hover-bg-blue-500 px-4 py-2 rounded-md duration-200">
             Learn More
           </button>
         </div>
