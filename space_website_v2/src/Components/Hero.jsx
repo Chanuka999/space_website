5f7fdc72e1be2a3eac01e344f5b801a406c07305
Chanuka999 <chanukaranditha99@gmail.com>
2025-03-04 19:00:34 +0530

    update Hero page

--- space_website_v2/src/Components/Hero.jsx
+++ space_website_v2/src/Components/Hero.jsx
@@ -25,8 +25,9 @@ const Hero = () => {
       <img
         src={MoonSurface}
         alt=""
-        className="absolute right-0 bottom-o w-full brightness-50 z-10"
+        className="absolute right-0 bottom-o w-full brightness-50 z-10 "
       />
+      <div className="absolute bottom-o w-full z-30 bg-gradient-to-b from-transparent from-10% to-black to-90% h-[20px] sm:h-[50px] md:[60px]"></div>
     </div>
   );
 };
