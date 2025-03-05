f8f4538e07dae33fe8e155fc8a5d2d2cf037db67
Chanuka999 <chanukaranditha99@gmail.com>
2025-03-05 09:09:42 +0530

    update

--- space_website_v2/src/Components/Banner.jsx
+++ space_website_v2/src/Components/Banner.jsx
@@ -3,15 +3,15 @@ import Satelite from "../assets/satelite1.jpg";
 
 const Banner = () => {
   return (
-    <div className="bg-black text-white pb-12">
+    <div className="bg-black text-white pb-12 relative z-50">
       <div className="container">
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 items-center">
           <div>
             <img src={Satelite} alt="" />
           </div>
-          <div>
-            <p>Our Mission</p>
-            <h1>Rapidcast</h1>
+          <div className="space-y-3 xl:pr-36 p-4 border-r-2 border-b-2 border-r-sky-800 border-b-sky-800">
+            <p className="text-sky-800 uppercase">Our Mission</p>
+            <h1 className="uppercase text-5xl">Rapidcast</h1>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
               ea a libero dolor aut ipsum, quo nesciunt, similique perferendis
