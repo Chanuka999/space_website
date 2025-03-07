cd53bf630a2d1e57c43630ee56339494526dfac0
Chanuka999 <chanukaranditha99@gmail.com>
2025-03-07 14:19:11 +0530

    final update

--- space_website_v2/src/Components/Banner2.jsx
+++ space_website_v2/src/Components/Banner2.jsx
@@ -7,18 +7,32 @@ const Banner2 = () => {
       <div className="container">
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 items-center">
           <div className="space-y-3 xl:pr-36 p-4 border-l-2 border-b-2 border-l-sky-800 border-b-sky-800">
-            <p className="text-sky-800 uppercase">Our Mission</p>
-            <h1 className="uppercase text-5xl">Rapidcast</h1>
-            <p>
+            <p data-aos="fade-up" className="text-sky-800 uppercase">
+              Our Mission
+            </p>
+            <h1
+              data-aos="zoom-in"
+              data-aos-delay="300"
+              className="uppercase text-5xl"
+            >
+              Rapidcast
+            </h1>
+            <p data-aos="zoom-in" data-aos-delay="500">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
               ea a libero dolor aut ipsum, quo nesciunt, similique perferendis
               dicta praesentium deleniti , aliquid sit alias quisquam. Vitae
               dolorem quam ducimus!
             </p>
-            <button className="primary-button">Learn More</button>
+            <button
+              data-aos="zoom-in"
+              data-aos-delay="700"
+              className="primary-button"
+            >
+              Learn More
+            </button>
           </div>
           <div>
-            <img src={Satelite} alt="" />
+            <img data-aos="zoom-in" src={Satelite} alt="" />
           </div>
         </div>
       </div>
